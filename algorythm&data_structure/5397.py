8ddd69a9cc8cc0bba6bb41d320966d51ac520cb9
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2026-03-13 23:47:20 +0900

    add solve

--- algorythm&data_structure/5397.py
+++ algorythm&data_structure/5397.py
@@ -1,3 +1,30 @@
 import sys
 
 input=sys.stdin.readline
+
+def solve():
+    N=int(input())
+    result=[]
+    for _ in range(N):
+        input_data = input().strip()
+        
+        left_stack=[]
+        right_stack=[]
+        
+        for i in input_data:
+            if i == "<":
+                if left_stack:
+                    right_stack.append(left_stack.pop())
+            elif i ==">":
+                if right_stack:
+                    left_stack.append(right_stack.pop())
+            elif i =="-":
+                if left_stack:
+                    left_stack.pop()
+            else:
+                left_stack.append(i)
+            
+        result="".join(left_stack)+"".join(reversed(right_stack))
+        sys.stdout.write(result+'\n')
+    
+solve()
